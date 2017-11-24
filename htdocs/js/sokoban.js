e19ecba7bb41b7269a6bf2ac046501d538d697d8
Alice105 <10515005@gm.fgu.edu.tw>
2017-11-24 16:02:43 +0800

    1124

---
+++ htdocs/js/sokoban.js
@@ -0,0 +1,11 @@
+/**
+ *  @file       index.js
+ *  The entry file of Sokoban.
+ */
+'use strict';
+
+window.addEventListener('load', () => {
+  console.log("sokoban.js loaded");
+});
+
+// index.js
